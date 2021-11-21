313b69c0b904ba98b605a1554457a96e4155fa7e
pavlov-web <al.pavlov.web@gmail.com>
2021-11-21 15:01:23 +0300

    del:(app.module) delete flug global from ConfigModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,9 +9,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-          isGlobal: true,
-      }),
+      ConfigModule.forRoot(),
       TypeOrmModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],
