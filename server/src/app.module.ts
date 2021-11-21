1c9051148536d307e304d90f50692a81ec309aad
pavlov-web <al.pavlov.web@gmail.com>
2021-11-21 14:46:46 +0300

    new:(auth) add JWT strategy

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,7 +9,9 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot(),
+      ConfigModule.forRoot({
+          isGlobal: true,
+      }),
       TypeOrmModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from "../user/user.service";
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+      private userService: UserService,
+      private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByCond({email, password})
@@ -13,5 +18,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: Omit<UserEntity, 'password'>) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      ...user,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
