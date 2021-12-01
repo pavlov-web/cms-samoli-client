cadcc88d657d2c5c98f98b77ad68f9f2d8ab8983
pavlov-web <al.pavlov.web@gmail.com>
2021-12-01 19:19:29 +0300

    ref:(file) refactor file modul

--- server/src/modules/file/file.controller.ts
+++ server/src/modules/file/file.controller.ts
@@ -5,9 +5,8 @@ import {
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
-import { FileService, MFile } from './file.service';
+import { FileService } from './file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { FileElementsResponse } from './dto/file.dto';
 
 @Controller('file')
 export class FileController {
@@ -15,44 +14,8 @@ export class FileController {
 
   @Post('upload')
   @HttpCode(200)
-  @UseInterceptors(FileInterceptor('files'))
-  async upload(
-    @UploadedFile() file: Express.Multer.File,
-  ): Promise<FileElementsResponse[]> {
-    const sizes = [1024, 768, 568, 100];
-    const saveArray: MFile[] = [new MFile(file)];
-
-    if (file.mimetype.includes('image')) {
-      const buffer = await this.fileService.convertToWebP(file.buffer);
-
-      for (const size of sizes) {
-        const bufferResize = await this.fileService.resizeImage(
-          file.buffer,
-          size,
-        );
-        const bufferResizeWebP = await this.fileService.resizeImage(
-          buffer,
-          size,
-        );
-
-        saveArray.push(
-          {
-            originalname: `${file.originalname.split('.')[0]}.webp`,
-            buffer: buffer,
-          },
-          {
-            originalname: `${file.originalname.split('.')[0]}_${size}.${
-              file.originalname.split('.')[1]
-            }`,
-            buffer: bufferResize,
-          },
-          {
-            originalname: `${file.originalname.split('.')[0]}_${size}.webp`,
-            buffer: bufferResizeWebP,
-          },
-        );
-      }
-    }
-    return this.fileService.save(saveArray);
+  @UseInterceptors(FileInterceptor('images'))
+  async upload(@UploadedFile() file: Express.Multer.File): Promise<any> {
+    return this.fileService.upload(file);
   }
 }
